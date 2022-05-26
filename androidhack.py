92991d50e20a9a384958a73d7361ff6cf5bfeec1
Shubham Omer <87802106+Shubhamomer@users.noreply.github.com>
2022-05-26 14:35:48 +0530

    Add files via upload

---
+++ androidhack.py
@@ -0,0 +1,5 @@
+             Android Phone Access Attackes
+             
+1. Wireless Attack l this attack use to connect vectem phone to Attacker phone in wifi,Tcp,usb,etc
+
+2. payload/backdoor l create backdoor 
